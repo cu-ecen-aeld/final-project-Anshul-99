b92e133479e07fbcd3fef906639e29fedf9bbf48
Anshul-99 <anshulsomani.99@gmail.com>
2022-04-22 21:39:35 -0600

    Final update

--- base_external/package/final-project/final-project.mk
+++ base_external/package/final-project/final-project.mk
@@ -5,8 +5,7 @@
 #
 ##############################################################
 
-#TODO: Fill up the contents below in order to reference your assignment 3 git contents
-FINAL_PROJECT_VERSION = 204153c5409c4af4fd123975cb86a422d4779b87 #Commit Version number
+FINAL_PROJECT_VERSION = 6b3b04a5299e5f927d29103ddb9bb395db710073 #Commit Version number
 # Note: Be sure to reference the *ssh* repository URL here (not https) to work properly
 # with ssh keys and the automated build/test system.
 # Your site should start with git@github.com:
@@ -16,16 +15,13 @@ FINAL_PROJECT_SITE_METHOD = git
 FINAL_PROJECT_GIT_SUBMODULES = YES
 
 define FINAL_PROJECT_BUILD_CMDS
-	$(MAKE) $(TARGET_CONFIGURE_OPTS) -C $(@D)/sensors all
-	#$(MAKE) $(TARGET_CONFIGURE_OPTS) -C $(@D)/client-vishal all
-	$(MAKE) $(TARGET_CONFIGURE_OPTS) -C $(@D)/server-new all
+	#$(MAKE) $(TARGET_CONFIGURE_OPTS) -C $(@D)/sensors all
+	$(MAKE) $(TARGET_CONFIGURE_OPTS) -C $(@D)/server all
 endef
 
-# TODO add your writer, finder and finder-test utilities/scripts to the installation steps below
 define FINAL_PROJECT_INSTALL_TARGET_CMDS
-	$(INSTALL) -m 0755 $(@D)/sensors/temp_humidity $(TARGET_DIR)/usr/bin
-	#$(INSTALL) -m 0755 $(@D)/client-vishal/socket_client $(TARGET_DIR)/usr/bin
-	$(INSTALL) -m 0755 $(@D)/server-new/server $(TARGET_DIR)/usr/bin
+	#$(INSTALL) -m 0755 $(@D)/sensors/light_sensor $(TARGET_DIR)/usr/bin
+	$(INSTALL) -m 0755 $(@D)/server/server $(TARGET_DIR)/usr/bin
 endef	
 
 $(eval $(generic-package))
